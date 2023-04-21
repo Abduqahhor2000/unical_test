e620bac4e22876bcccad366a94c6e4203a4f91ed
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2023-04-22 02:46:26 +0500

    fix server

--- src/store/features/apiSlice.tsx
+++ src/store/features/apiSlice.tsx
@@ -17,7 +17,7 @@ export interface Location {
 
 export const tableDataApi = createApi({
   reducerPath: 'tableDataApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://unical-test.vercel.app' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://unical-test.vercel.app/api' }),
   tagTypes: ["TableData"],
   endpoints: (builder) => ({
     getTable: builder.query<TransferInfo[], void>({
